58f6676b0b5dd8b737bde52f33bde2245e38ad73
Eti <135351590+eticau@users.noreply.github.com>
2024-10-28 20:34:59 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,88 @@
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
+// ——————————————————————————————————————————————————
+// Example
+// ——————————————————————————————————————————————————
+
+const phrases = [
+  'Skills',
+  'comer',
+  'dormir',
+  'I’m a digital enthusiast who enjoys technology and marketing. I specialise in digital marketing, no-code, automation, and Webflow. I’m passionate about what I do - all while having a lot of fun along the way I’m committed to being the go-to for digital innovation in the tech and marketing space. I work with businesses and founders who are seeking better ways to engage their customers and achieve their goals. My goal is simple to consistently produce high quality work which accelerates client’s growth and takes them to new heights',
+  'Work inquiries, commission requeats, speaking inquiries, funny jokes, and just-saying-hi’s can be sent to:',
+  'Este es mi sitio web. Soy comediante. Me interesa el cambio. Me interesa el despertar.',
+  'Swing by for a cup of coffee, or leave me a note:'
+]
+
+const el = document.querySelector('.text3')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
+
 // about me
 // set up text to print, each item in array is new line
 var aText = new Array(
